0eced5925bc1dbc456337fd04b7b860dc109a4aa
Avishay7 <avishayh2345@gmail>
2024-09-30 12:05:38 +0300

    avatar/chet

--- src/componentsClient/chat.jsx
+++ src/componentsClient/chat.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Chat() {
   return (
     <div>
-      
+      <div className="container d-flex justify-content-center mt-5">
+      <div className="card" style={{ width: '22rem' }}>
+        <div className="card-body d-flex flex-column" style={{ height: '500px', overflowY: 'auto' }}>
+          <div className="d-flex justify-content-start mb-3">
+            <div className="p-2 bg-light border rounded">שלום! איך אני יכול לעזור לך?</div>
+          </div>
+          <div className="d-flex justify-content-end mb-3">
+            <div className="p-2 bg-success text-white rounded">אני רוצה ללמוד אנגלית .</div>
+          </div>
+          <div className="d-flex justify-content-start mb-3">
+            <div className="p-2 bg-light border rounded">הנה דוגמה פשוטה באמצעות Bootstrap.</div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
