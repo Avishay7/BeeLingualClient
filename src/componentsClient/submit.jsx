5924ae07c88b997a4605c89acc91c00275d7527b
shlomosharabi <shlomosh1999@gmail.com>
2024-09-30 13:09:09 +0300

    change bg

--- src/componentsClient/submit.jsx
+++ src/componentsClient/submit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Submit() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+    <div className="d-flex justify-content-center align-items-center vh-100 ">
       <div className="border border-dark rounded p-4 d-flex flex-column" style={{ width: '80%', maxWidth: '500px', backgroundColor: 'white' }}>
         <h2 className="text-center mb-4" style={{ fontSize: '1.6rem' }}>Reset your password</h2>
         <p className="text-center mb-5" style={{ fontSize: '1rem' }}>Enter your account Email address to receive a security code</p>
