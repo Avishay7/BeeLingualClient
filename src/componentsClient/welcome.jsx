0df2d9fa313fdc8ffadcd4a67626110ebdbfec45
shimondriham <95618903+shimondriham@users.noreply.github.com>
2024-10-07 23:13:43 +0300

    welcom

--- src/componentsClient/welcome.jsx
+++ src/componentsClient/welcome.jsx
@@ -12,44 +12,46 @@ const Welcome = () => {
 
     return (
         <div className="container mt-5">
-            <div className="d-flex justify-content-lg-around p-2 m-3 rounded" style={{ height: '350px' }}>
-
-                <div className="row" style={{ height: '50px' }}>
-                    <button onClick={toSignIn} className="btn btn-outline-primary m-3">Sign In</button>
-                    <button onClick={toSignUp} className="btn btn-outline-primary m-3">Sign Up</button>
+            <div className="d-flex justify-content-around p-5 m-3" style={{ height: '350px' }}>
+                <div className="text-center p-3" style={{ height: '100%', width: "40%" }}>
+                    <div className="m-3">logo</div>
+                    <button onClick={toSignIn} className="btn btn-outline-primary m-3 w-50" >Sign In</button>
+                    <button onClick={toSignUp} className="btn btn-outline-primary m-3 w-50">Sign Up</button>
                 </div>
 
-                <div>
-                    <div className="bg-white p-3 shadow-lg rounded" style={{ height: '200px' }}>
-                        <h3>About Us</h3>
+                <div className="px-4" style={{ height: '100%', width: "60%" }}  >
+                    <div className="bg-white p-4 shadow-lg text-center" style={{ height: '100%', borderRadius: '16px' }}>
+                        <h3 className="py-2">About Us</h3>
+                        <p>We are a company focused on providing the best services...</p>
+                        <p>We are a company focused on providing the best services...</p>
+                        <p>We are a company focused on providing the best services...</p>
                         <p>We are a company focused on providing the best services...</p>
                     </div>
                 </div>
             </div>
 
-            <div className="row">
-                <div className="col-12">
-                    <div className="row justify-content-center">
-                        <div className="col-3">
-                            <div className="p-3 m-2 shadow-sm rounded bg-white">
-                                <h5>Recommendation 1</h5>
-                                <p>"Great service! Highly recommended."</p>
-                            </div>
-                        </div>
+            <div className=" row justify-content-center text-center m-4">
+                <div className="col-3">
+                    <div className="p-3 m-2 shadow-lg  bg-white" style={{minHeight: '150px', borderRadius: '16px' }}>
+                        <div>image</div>
+                        <h5>Recommendation 1</h5>
+                        <p>"Great service! Highly recommended."</p>
+                    </div>
+                </div>
 
-                        <div className="col-3"> 
-                            <div className="p-3 m-2 shadow-sm rounded bg-white">
-                                <h5>Recommendation 2</h5>
-                                <p>"Exceptional quality and customer support."</p>
-                            </div>
-                        </div>
+                <div className="col-3">
+                    <div className="p-3 m-2 shadow-sm  bg-white" style={{minHeight: '150px', borderRadius: '16px' }}>
+                        <div>image</div>
+                        <h5>Recommendation 2</h5>
+                        <p>"Exceptional quality and customer support."</p>
+                    </div>
+                </div>
 
-                        <div className="col-3">
-                            <div className=" p-3 m-2 shadow-sm rounded bg-white">
-                                <h5>Recommendation 3</h5>
-                                <p>"Fast delivery and reliable services."</p>
-                            </div>
-                        </div>
+                <div className="col-3">
+                    <div className="p-3 m-2 shadow-sm  bg-white" style={{minHeight: '150px', borderRadius: '16px' }}>
+                        <div>image</div>
+                        <h5>Recommendation 3</h5>
+                        <p>"Fast delivery and reliable services."</p>
                     </div>
                 </div>
             </div>
