7353a820316034cb0e19cef01728f76ceb80eadf
Shmueltzadok <shmuel770tz@gmail.com>
2024-10-07 12:35:59 +0300

    forgotpass1

--- src/componentsClient/forgotPass.jsx
+++ src/componentsClient/forgotPass.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 function ForgotPass() {
   return (
-    <div>
-      forgot Pass
+    <>
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4 shadow-sm" style={{ width: '350px', borderRadius: '10px' }}>
+        <h3 className="text-center mb-4">forgot-Paasword</h3>
+        <form>
+          <div className="form-group mb-3">
+            <label>new password</label>
+            <input type="password" className="form-control" required />
+          </div>
+          <div className="form-group mb-3">
+            <label>confirme password</label>
+            <input type="password" className="form-control" required />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Continue</button>
+        </form>
+      </div>
     </div>
+    </>
   )
 }
 
